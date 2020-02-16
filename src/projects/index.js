cd2c388cc393ba0687172f85c28e799e9e8d076d
Rafael Barash <rafabarash@gmail.com>
2020-02-16 12:20:08 -0600

    adding checkpoint two to portfolio

--- src/projects/index.js
+++ src/projects/index.js
@@ -1,6 +1,7 @@
 import checkpoint1Metadata from "./proj1/checkpointMetadata";
 import proj1Metadata from "./proj1/projMetadata";
+import checkpoint2Metadata from "./proj2/checkpointMetadata";
 
-const Projects = [proj1Metadata, checkpoint1Metadata];
+const Projects = [checkpoint2Metadata, proj1Metadata, checkpoint1Metadata];
 
 export default Projects;
