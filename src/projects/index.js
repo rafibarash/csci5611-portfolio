b54aa5048773a90fa9be3e9fae21f7ad8fa0326c
Rafael Barash <rafabarash@gmail.com>
2020-02-25 11:33:26 -0600

    started on proj2 metadata

--- src/projects/index.js
+++ src/projects/index.js
@@ -1,7 +1,15 @@
 import checkpoint1Metadata from "./proj1/checkpointMetadata";
 import proj1Metadata from "./proj1/projMetadata";
 import checkpoint2Metadata from "./proj2/checkpointMetadata";
+import clothSimMetadata from "./proj2/clothSimMetadata";
+import fluidSimMetadata from "./proj2/fluidSimMetadata";
 
-const Projects = [checkpoint2Metadata, proj1Metadata, checkpoint1Metadata];
+const Projects = [
+  clothSimMetadata,
+  fluidSimMetadata,
+  checkpoint2Metadata,
+  proj1Metadata,
+  checkpoint1Metadata,
+];
 
 export default Projects;
